6a33f1f1dd366c890e6afe814db2ffaccb0c782c
Kira <gajdukkira232@gmail.com>
2026-02-07 15:01:05 +0200

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'        // <- ОБОВ'ЯЗКОВО
+import react from '@vitejs/plugin-react'  // <- для React
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/homework-react/',  // <- важливо для GitHub Pages
   plugins: [react()],
 })
-
-
